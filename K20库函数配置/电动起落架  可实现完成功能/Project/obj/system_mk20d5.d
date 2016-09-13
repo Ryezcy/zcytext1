d811be37b349b33c48a6d1938db9ba1d12d786d2
zcy <458153458@qq.com>
2016-09-13 10:20:42 +0800

    加入忽略

--- K20库函数配置/电动起落架  可实现完成功能/Project/obj/system_mk20d5.d
+++
@@ -1,8 +0,0 @@
-.\obj\system_mk20d5.o: ..\Startup\system_MK20D5.c
-.\obj\system_mk20d5.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\obj\system_mk20d5.o: ..\CMSIS\MK20D5.h
-.\obj\system_mk20d5.o: ..\CMSIS\core_cm4.h
-.\obj\system_mk20d5.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
-.\obj\system_mk20d5.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
-.\obj\system_mk20d5.o: C:\Keil\ARM\CMSIS\Include\core_cm4_simd.h
-.\obj\system_mk20d5.o: ..\Startup\system_MK20D5.h
--- K20库函数配置/电动起落架  可实现完成功能/Project/obj/systick.d
+++
@@ -1,9 +0,0 @@
-.\obj\systick.o: ..\Drivers\systick.c
-.\obj\systick.o: ..\Drivers\systick.h
-.\obj\systick.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\obj\systick.o: ..\CMSIS\MK20D5.h
-.\obj\systick.o: ..\CMSIS\core_cm4.h
-.\obj\systick.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
-.\obj\systick.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
-.\obj\systick.o: C:\Keil\ARM\CMSIS\Include\core_cm4_simd.h
-.\obj\systick.o: ..\Startup\system_MK20D5.h
